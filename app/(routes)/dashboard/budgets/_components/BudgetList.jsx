22eb143a8adecd84fc886e5855bd8478e760bd61
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2024-05-03 09:56:03 +0600

    Adding create budget functions

--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import CreateBudget from "./CreateBudget";
 
 const BudgetList = () => {
-  return <div>
-    <CreateBudget/>
-  </div>;
+  return (
+    <div className="mt-7">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <CreateBudget />
+      </div>
+    </div>
+  );
 };
 
 export default BudgetList;
--- app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import EmojiPicker from "emoji-picker-react";
 
 const CreateBudget = () => {
-  return <div>Createbudgets</div>;
+  const [emojiIcon, setEmojiIcon] = useState("😀");
+  const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger>
+          <div className="bg-slate-300 p-10 rounded-md items-center flex flex-col border-2 border-dashed cursor-pointer hover:shadow-md">
+            <h2 className="text-3xl">+</h2>
+            <h2>Create New Budget</h2>
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Budget</DialogTitle>
+            <DialogDescription>
+              <div className="mt-5">
+                <Button
+                  className="text-lg"
+                  variant="outline"
+                  onClick={() => setOpenEmojiPicker(!openEmojiPicker)}
+                >
+                  {emojiIcon}
+                </Button>
+                <div className="absolute z-20">
+                  <EmojiPicker
+                    open={openEmojiPicker}
+                    onEmojiClick={(e) => {
+                      setEmojiIcon(e.emoji);
+                      setOpenEmojiPicker(false);
+                    }}
+                  />
+                </div>
+                <div className="mt-2">
+                  <h2 className="text-black font-medium my-1">Budget Name</h2>
+                  <Input
+                    placeholder="e.g. Home Decor"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mt-2">
+                  <h2 className="text-black font-medium my-1">Budget Amount</h2>
+                  <Input
+                    placeholder="e.g. 5000 Bdt"
+                    type="number"
+                    onChange={(e) => setAmount(e.target.value)}
+                  />
+                </div>
+                <Button
+                  className="mt-5 w-full"
+                  disabled={!(name && amount)}
+                  onClick={() => onCreateBudget()}
+                >
+                  Create Budget
+                </Button>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
 
 export default CreateBudget;
